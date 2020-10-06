15f4a9b38892c1c8df6e1bf64b92266388e85970
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-10-05 21:03:52 -0500

    DB Setup

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Beef Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bison Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Portobello Mushroom Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Turkey Burger", false);
