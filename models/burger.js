ce52a7c5f1da9ee32b5b432dcff63aa85f4b10d2
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-10-07 19:29:41 -0500

    Updated insertOne and updateOne functions

--- models/burger.js
+++ models/burger.js
@@ -14,8 +14,8 @@ var burger = {
         });
     },
 
-    updateOne: function(objColVals, condition, cb) {
-        orm.updateOne("burgers", objColVals, condition, function(res) {
+    updateOne: function(val, cb) {
+        orm.updateOne("burgers", val, function(res) {
             cb(res);
         });
     }
