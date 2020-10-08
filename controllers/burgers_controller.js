a8b7811dbe731490dbf33a526452a83a99decba4
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-10-07 19:14:54 -0500

    Updated post and put routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,11 +2,11 @@ var express = require("express");
 
 var router = express.Router();
 
-var burger = require("../models/burger.js");
+const burger = require("../models/burger.js");
 
 router.get("/", function(req,res) {
     burger.selectAll(function(data) {
-        var handlebarsObj = {
+        let handlebarsObj = {
             burgers: data
         };
         console.log(handlebarsObj);
@@ -15,21 +15,14 @@ router.get("/", function(req,res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-    burger.insertOne([
-        "burger_name", "devoured"
-    ], [
-        req.body.name, req.body.devoured
-    ], function(result) {
+    burger.insertOne(["burger_name"],[req.body.burger_name], function(result) {
         res.json({id: result.insertId});
+        console.log("created new burger");
     });
 });
 
 router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-    
-    burger.updateOne({
-        devoured: req.body.devoured
-    }, condition, function(result) {
+    burger.updateOne(req.params.id, function(result) {
         if (result.changedRows == 0) {
             return res.status(404).end();
         }
